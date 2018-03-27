7bc7eb40913b213bb919965c5c53b0cb0c0db8f9
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-27 19:44:58 +0000

    minor code revisions after code review

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,59 +6,89 @@
 
 $(document).ready(function(){
    // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
+   // Fake data taken from tweets.json
+   const data = [
+     {
+       "user": {
+         "name": "Newton",
+         "avatars": {
+           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+         },
+         "handle": "@SirIsaac"
+       },
+       "content": {
+         "text": "If I have seen further it is by standing on the shoulders of giants"
+       },
+       "created_at": 1461116232227
+     },
+     {
+       "user": {
+         "name": "Descartes",
+         "avatars": {
+           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+         },
+         "handle": "@rd" },
+       "content": {
+         "text": "Je pense , donc je suis"
+       },
+       "created_at": 1461113959088
+     },
+     {
+       "user": {
+         "name": "Johann von Goethe",
+         "avatars": {
+           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+         },
+         "handle": "@johann49"
+       },
+       "content": {
+         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+       },
+       "created_at": 1461113796368
+     }
+   ];
 
-
-  // <article class="tweet">
-  //   <header>
-  //     <img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png" />
-  //     <span class="username">Mr Popular</span>
-  //     <span class="handle">@MrPop</span>
-  //   </header>
-  //   <div>I have so many important things to say!</div>
-  //   <footer>10 Days Ago<span>&#9873; &#11156; &#9829;</span></footer>
-  // </article>
+  // creates and returns element tree for a single tweet
   function createTweetElement(tweetData){
 
+    // create element nodes
     let $article = $("<article>").addClass("tweet");
     let $header = $("<header>");
     let $avatar = $("<img>").attr("src", tweetData.user.avatars.regular);
     let $username = $("<span>").addClass("username").text(tweetData.user.name);
     let $handle = $("<span>").addClass("handle").text(tweetData.user.handle);
     let $tweet = $("<div>").text(tweetData.content.text);
-    let $footer = $("<footer>").text(tweetData.created_at)
+    let $footer = $("<footer>");
 
+    // append nodes to create tree off article node
     $header.appendTo($article);
     $avatar.appendTo($header);
     $username.appendTo($header);
     $handle.appendTo($header);
-
     $tweet.appendTo($article);
     $footer.appendTo($article);
-    $("<span>&#9873; &#11156; &#9829;</span>").appendTo($footer);
-    debugger;
+
+    // add footer data
+    $("<span>" + Math.round(((Date.now() - tweetData.created_at) / 86400000)) + " days ago" + "</span>").appendTo($footer);
+    $("<span>&#9873; &#11156; &#9829;</span>").addClass("actions").appendTo($footer);
+
+    // returns complete tree of jQuery objects, ready for insertion into the DOM.
     return $article;
   }
 
-  let $tweet = createTweetElement(tweetData);
+  // generates and inserts DOM nodes for tweets
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweetData){
+      $('#tweets-container').append(createTweetElement(tweetData));
+    });
+  }
 
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 
 });
