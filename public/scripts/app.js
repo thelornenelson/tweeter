533f57f0ee5ae3fb55ad3eeed2782fb7abb1fea0
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-28 18:44:28 +0000

    implemented tweet fetching via AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,61 +7,63 @@
 $(document).ready(function(){
    // Test / driver code (temporary). Eventually will get this from the server.
    // Fake data taken from tweets.json
-   const data = [
-     {
-       "user": {
-         "name": "Newton",
-         "avatars": {
-           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-         },
-         "handle": "@SirIsaac"
-       },
-       "content": {
-         "text": "If I have seen further it is by standing on the shoulders of giants"
-       },
-       "created_at": 1461116232227
-     },
-     {
-       "user": {
-         "name": "Descartes",
-         "avatars": {
-           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-         },
-         "handle": "@rd" },
-       "content": {
-         "text": "Je pense , donc je suis"
-       },
-       "created_at": 1461113959088
-     },
-     {
-       "user": {
-         "name": "Johann von Goethe",
-         "avatars": {
-           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-         },
-         "handle": "@johann49"
-       },
-       "content": {
-         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-       },
-       "created_at": 1461113796368
-     }
-   ];
+   // const data = [
+   //   {
+   //     "user": {
+   //       "name": "Newton",
+   //       "avatars": {
+   //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+   //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+   //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+   //       },
+   //       "handle": "@SirIsaac"
+   //     },
+   //     "content": {
+   //       "text": "If I have seen further it is by standing on the shoulders of giants"
+   //     },
+   //     "created_at": 1461116232227
+   //   },
+   //   {
+   //     "user": {
+   //       "name": "Descartes",
+   //       "avatars": {
+   //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+   //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+   //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+   //       },
+   //       "handle": "@rd" },
+   //     "content": {
+   //       "text": "Je pense , donc je suis"
+   //     },
+   //     "created_at": 1461113959088
+   //   },
+   //   {
+   //     "user": {
+   //       "name": "Johann von Goethe",
+   //       "avatars": {
+   //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+   //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+   //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+   //       },
+   //       "handle": "@johann49"
+   //     },
+   //     "content": {
+   //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+   //     },
+   //     "created_at": 1461113796368
+   //   }
+   // ];
 
   // Handle form submission
   $("form").on("submit", function(event){
     event.preventDefault();
     let data = $(this).serialize();
     $.post("/tweets", data).done(function(response){
-      console.log(response);
+      loadTweets();
     });
   });
+
+
 
   // creates and returns element tree for a single tweet
   function createTweetElement(tweetData){
@@ -98,6 +100,13 @@ $(document).ready(function(){
     });
   }
 
-  renderTweets(data);
+  // retrieve tweets from server
+  function loadTweets(){
+    $.getJSON("/tweets").done(function(tweets){
+      renderTweets(tweets);
+    })
+  }
+
+  loadTweets();
 
 });
