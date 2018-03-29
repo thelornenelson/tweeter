22f9bd75d21d549ad862c57a8e3bb5ba5f4d34d6
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-29 20:24:10 +0000

    implemented write to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -3,18 +3,12 @@ const assert = require("assert");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
-  function getTweets(callback){
-    db.collection("tweets").find().toArray(callback);
-  }
 
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+        db.collection("tweets").insertOne(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
